c7babb45c304229439e4648451d93ddf42b3416f
Rawda Yasser <rawdayasser2019@gmail.com>
2019-10-28 22:33:20 +0200

    Added basic thread

--- app/src/main/java/com/rawda/threads_code_sample/MainActivity.java
+++ app/src/main/java/com/rawda/threads_code_sample/MainActivity.java
@@ -3,12 +3,24 @@ package com.rawda.threads_code_sample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startThread();
+    }
+    private void startThread(){
+        for  (int i = 0; i < 10; i++){
+            try {
+                Thread.sleep(1000);
+                Log.i(TAG, "startThread: " + i);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
